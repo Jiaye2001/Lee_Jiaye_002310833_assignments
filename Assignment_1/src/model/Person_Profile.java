8ad40454009eaea0c7ef2bf8050a8b69ac59f239
Jiaye Lee <lee.jiay@northeastern.edu>
2024-09-18 11:00:54 -0400

    Added Assignment_1

---
+++ Assignment_1/src/model/Person_Profile.java
@@ -0,0 +1,193 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author jiaye
+ */
+public class Person_Profile {
+    String first_name;
+    String last_name;
+    String email;
+    String phone;
+    String driver_lice_num;
+    String drive_lice_state;
+    String ssn;
+    String addr_1;
+    String addr_2;
+    String city;
+    String state;
+    String zip;
+    String tele_num;
+    String fax_num;
+    String hobby;
+    String gender;
+    String mbti;
+    String star_sign;
+    String status;
+    String age;
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getDriver_lice_num() {
+        return driver_lice_num;
+    }
+
+    public void setDriver_lice_num(String driver_lice_num) {
+        this.driver_lice_num = driver_lice_num;
+    }
+
+    public String getDrive_lice_state() {
+        return drive_lice_state;
+    }
+
+    public void setDrive_lice_state(String drive_lice_state) {
+        this.drive_lice_state = drive_lice_state;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public String getAddr_1() {
+        return addr_1;
+    }
+
+    public void setAddr_1(String addr_1) {
+        this.addr_1 = addr_1;
+    }
+
+    public String getAddr_2() {
+        return addr_2;
+    }
+
+    public void setAddr_2(String addr_2) {
+        this.addr_2 = addr_2;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getTele_num() {
+        return tele_num;
+    }
+
+    public void setTele_num(String tele_num) {
+        this.tele_num = tele_num;
+    }
+
+    public String getFax_num() {
+        return fax_num;
+    }
+
+    public void setFax_num(String fax_num) {
+        this.fax_num = fax_num;
+    }
+
+    public String getHobby() {
+        return hobby;
+    }
+
+    public void setHobby(String hobby) {
+        this.hobby = hobby;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getMbti() {
+        return mbti;
+    }
+
+    public void setMbti(String mbti) {
+        this.mbti = mbti;
+    }
+
+    public String getStar_sign() {
+        return star_sign;
+    }
+
+    public void setStar_sign(String star_sign) {
+        this.star_sign = star_sign;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+    
+}
